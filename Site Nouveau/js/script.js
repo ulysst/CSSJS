2c43c03de01fc9d1598d213789d8026cb2198969
unknown <utrachet@gamingcampus.local>
2021-11-18 15:43:35 +0100

    Contact

--- Site Nouveau/js/script.js
+++ Site Nouveau/js/script.js
@@ -1,3 +1,12 @@
   $(document).ready(function(){
     $('.parallax').parallax();
   });
+
+ $(document).ready(function(){
+  $('.carousel').carousel();
+});
+
+ $(document).ready(function(){
+   $('.modal').modal();
+ });
+         
