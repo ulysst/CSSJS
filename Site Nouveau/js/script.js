91e7cbe078f0e727abae6d3184bc34968d14ff0d
unknown <utrachet@gamingcampus.local>
2021-11-19 10:50:28 +0100

    Navbar/img

--- Site Nouveau/js/script.js
+++ Site Nouveau/js/script.js
@@ -1,12 +1,24 @@
-  $(document).ready(function(){
+  $(document).ready(function() {
     $('.parallax').parallax();
   });
 
- $(document).ready(function(){
-  $('.carousel').carousel();
-});
+  $(document).ready(function() {
+    $('.carousel').carousel();
+  });
 
- $(document).ready(function(){
-   $('.modal').modal();
- });
-         
+  $(document).ready(function() {
+    $('.modal').modal();
+  });
+
+  $(document).ready(function() {
+    $('.collapsible').collapsible();
+  });
+
+  $(document).ready(function() {
+    $('.sidenav').sidenav();
+  });
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+          
